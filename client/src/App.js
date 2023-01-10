d8121caf91bd36c65c48ff62f6ec14c42d3ebad6
chyerin802 <chyerin802@gmail.com>
2023-01-10 14:20:47 +0900

    fix: Container 스타일 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,17 @@
 import Header from './components/Header';
-import styled from 'styled-components';
 import GlobalStyle from './GlobalStyle';
 import { Routes, Route } from 'react-router-dom';
 
 import FindMatePage from './pages/FindMatePage';
-
-const Container = styled.div`
-  padding-top: var(--header-height);
-  background-color: var(--bg-dark-color);
-  width: 100%;
-  max-width: 980px;
-  height: 100%;
-  color: var(--main-color);
-`;
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Header></Header>
-      <Container>
-        <Routes>
-          <Route path="/mate" element={<FindMatePage />}></Route>
-        </Routes>
-      </Container>
+      <Routes>
+        <Route path="/mate" element={<FindMatePage />}></Route>
+      </Routes>
     </div>
   );
 }
