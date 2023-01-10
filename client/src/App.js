1e5044396aed4d0e8e15ff4fabe6b329da0acd4e
operat04 <operat04@naver.com>
2023-01-10 21:46:18 +0900

    [FE] feat : 유저 정보 입력 페이지 구현

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,17 @@ import GlobalStyle from './GlobalStyle';
 import { Routes, Route } from 'react-router-dom';
 import FindMatePage from './pages/FindMatePage';
 import SignUpPage from './pages/SignUpPage';
+import InputUserInfoPage from './pages/InputUserInfoPage';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Header></Header>
-
       <Routes>
         <Route path="/mate" element={<FindMatePage />}></Route>
+        <Route path="/signup" element={<SignUpPage />}></Route>
+        <Route path="/inputuserinfo" element={<InputUserInfoPage />}></Route>
       </Routes>
     </div>
   );
