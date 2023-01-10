b8e26e2b8cb3b0236bfa4c6c2f155857a2686270
jjunghakim <wlikeoxy@gmail.com>
2023-01-10 17:35:01 +0900

    #49 [BE]fix: 댓글좋아요 엔티티 수정

--- server/src/main/java/com/mainproject/server/domain/comments/entity/CommentsLike.java
+++ server/src/main/java/com/mainproject/server/domain/comments/entity/CommentsLike.java
@@ -1,25 +1,42 @@
 package com.mainproject.server.domain.comments.entity;
 
-import javax.persistence.Column;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import com.mainproject.server.audit.Auditable;
+import com.mainproject.server.domain.member.entity.Member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CommentsLike extends Auditable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long commentLikeId;
 
-	@Column(nullable = false)
-	private Long memberId;
+	// 좋아요 ~ 멤버 (N : 1)
+	@ManyToOne(cascade = CascadeType.DETACH)
+	@JoinColumn(name = "member_id")
+	private Member member;
 
-	@Column(nullable = false)
-	private Long commentsId;
+	//좋아요 ~ 댓글 (N : 1)
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "comments_id")
+	private Comments comments;
 
-	//@ManyToOne
-
+	public CommentsLike(Member member, Comments comments){
+		this.member = member;
+		this.comments = comments;
+	}
 }
