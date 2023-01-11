a345c51460d68133a4ac9340b71153d35eae98c3
jjunghakim <wlikeoxy@gmail.com>
2023-01-11 14:32:58 +0900

    #59 [BE]feat: 댓글 좋아요 구현

--- server/src/main/java/com/mainproject/server/domain/comments/entity/CommentsLike.java
+++ server/src/main/java/com/mainproject/server/domain/comments/entity/CommentsLike.java
@@ -2,6 +2,8 @@ package com.mainproject.server.domain.comments.entity;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -18,25 +20,39 @@ import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommentsLike extends Auditable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long commentLikeId;
+	private Long commentsLikeId;
 
 	// 좋아요 ~ 멤버 (N : 1)
-	@ManyToOne(cascade = CascadeType.DETACH)
-	@JoinColumn(name = "member_id")
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "member_id", nullable = false)
 	private Member member;
 
 	//좋아요 ~ 댓글 (N : 1)
 	@ManyToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name = "comments_id")
+	@JoinColumn(name = "comments_id", nullable = false)
 	private Comments comments;
 
-	public CommentsLike(Member member, Comments comments){
-		this.member = member;
-		this.comments = comments;
+	@Enumerated(EnumType.STRING)
+	private LikeStatus likeStatus = LikeStatus.LIKE;
+
+	public enum LikeStatus {
+		LIKE("등록"),
+		CANCEL("취소");
+
+		@Getter
+		private String likeStatus;
+
+		LikeStatus(String likeStatus) {
+			this.likeStatus = likeStatus;
+		}
+	}
+	public CommentsLike(Long commentsLikeId){
+		this.commentsLikeId = commentsLikeId;
 	}
 }
