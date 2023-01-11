c717220d6beee8ca09c5c608e6732ad2e8e8fb7e
syk506 <suuunyy0@naver.com>
2023-01-11 11:08:09 +0900

    [FE] design: 전체 디자인 수정

---
+++ client/src/components/Container.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding-top: var(--header-height);
+  background-color: var(--bg-dark-color);
+  width: 100%;
+  max-width: 1200px;
+  height: 100%;
+  color: var(--main-color);
+`;
+
+export default Container;
