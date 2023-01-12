7af511fe0b4b89720947d28a39e491cc2deed208
operat04 <operat04@naver.com>
2023-01-12 10:45:51 +0900

    [FE] feat : 마이 페이지 강아지 정보 추가

---
+++ client/src/static/dummyMyPetinfo.js
@@ -0,0 +1,52 @@
+export const petInfo = [
+  {
+    id: 1,
+    name: '둥이',
+    age: '10',
+    male: true,
+    breed: '말티즈',
+    Introduce: '낯가림이 많고 산책을 오래하면 안걸음.',
+    pet_img:
+      'https://i.ibb.co/Rj5b3xs/Kakao-Talk-Photo-2023-01-12-00-46-38.jpg',
+  },
+  {
+    id: 2,
+    name: '둥이',
+    age: '10',
+    male: true,
+    breed: '말티즈',
+    Introduce: '낯가림이 많고 산책을 오래하면 안걸음.',
+    pet_img:
+      'https://i.ibb.co/Rj5b3xs/Kakao-Talk-Photo-2023-01-12-00-46-38.jpg',
+  },
+  {
+    id: 3,
+    name: '둥이',
+    age: '10',
+    male: true,
+    breed: '말티즈',
+    Introduce: '낯가림이 많고 산책을 오래하면 안걸음.',
+    pet_img:
+      'https://i.ibb.co/Rj5b3xs/Kakao-Talk-Photo-2023-01-12-00-46-38.jpg',
+  },
+  {
+    id: 4,
+    name: '둥이',
+    age: '10',
+    male: true,
+    breed: '말티즈',
+    Introduce: '낯가림이 많고 산책을 오래하면 안걸음.',
+    pet_img:
+      'https://i.ibb.co/Rj5b3xs/Kakao-Talk-Photo-2023-01-12-00-46-38.jpg',
+  },
+  {
+    id: 5,
+    name: '둥이',
+    age: '10',
+    male: true,
+    breed: '말티즈',
+    Introduce: '낯가림이 많고 산책을 오래하면 안걸음.',
+    pet_img:
+      'https://i.ibb.co/Rj5b3xs/Kakao-Talk-Photo-2023-01-12-00-46-38.jpg',
+  },
+];
