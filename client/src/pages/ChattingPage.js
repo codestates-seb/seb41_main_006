8c7f2c61c0a27b57d2e17ee7c33c14251de7943e
chyerin802 <chyerin802@gmail.com>
2023-01-13 11:00:22 +0900

    [FE] design: 채팅 페이지

---
+++ client/src/pages/ChattingPage.js
@@ -0,0 +1,17 @@
+import Container from '../components/Container';
+import styled from 'styled-components';
+
+const ChattingPageContainer = styled(Container)`
+  display: flex;
+  background-color: var(--bg-dark-color);
+`;
+const ChattingPage = () => {
+  return (
+    <ChattingPageContainer>
+      <div>hihihih</div>
+      <div>jhihih</div>
+    </ChattingPageContainer>
+  );
+};
+
+export default ChattingPage;
