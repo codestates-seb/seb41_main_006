3cc0c350ed881f91fe679e04e874cf7714339c57
operat04 <operat04@naver.com>
2023-01-13 10:47:08 +0900

    [FE]Design : 유저 정보 모달 마크업

---
+++ client/src/components/myPage/PetInfoCard.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
+  color: var(--main-font-color);
+  > * {
+    margin: 1% 0;
+  }
+  img {
+    width: 15rem;
+    height: 12rem;
+    border-radius: 10px;
+  }
+  .Info {
+    display: flex;
+    > div {
+      text-align: center;
+    }
+    > :nth-child(2) {
+      border-left: 1px solid black;
+      border-right: 1px solid black;
+      border-color: var(--main-font-color);
+      margin: 0 10px;
+      padding: 0 10px;
+    }
+  }
+  .Introduce {
+  }
+`;
+
+const PetInfoCard = ({ pet }) => {
+  return (
+    <CardContainer>
+      <h2>강아지 소개</h2>
+      <img src={pet.pet_img} alt=""></img>
+      <div>{pet.name}</div>
+      <div className="Info">
+        <div>🐶 {pet.age}살</div>
+        <div>{pet.male ? '수컷' : '암컷'}</div>
+        <div>{pet.breed}</div>
+      </div>
+      <div className="Introduce">{pet.Introduce}</div>
+    </CardContainer>
+  );
+};
+
+export default PetInfoCard;
