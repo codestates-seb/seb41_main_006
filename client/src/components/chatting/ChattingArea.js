6bd8a0dc69071a56e016a99ec44a1f54878e0521
chyerin802 <chyerin802@gmail.com>
2023-01-13 16:21:17 +0900

    [FE] design: 채팅 리스트, 채팅방 마크업

---
+++ client/src/components/chatting/ChattingArea.js
@@ -0,0 +1,13 @@
+import MyChat from './MyChat';
+import Chat from './Chat';
+
+const ChattingArea = () => {
+  return (
+    <>
+      <MyChat></MyChat>
+      <Chat />
+    </>
+  );
+};
+
+export default ChattingArea;
