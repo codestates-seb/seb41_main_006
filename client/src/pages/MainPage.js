6909c7b1d8b24bcc7b8fd178ba4b106d5a701889
operat04 <operat04@naver.com>
2023-01-17 17:01:37 +0900

    [FE] DESIGN : 메인 페이지 마크업

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,7 +1,24 @@
+import MainFirstPage from '../components/MainPage/MainFirstPage';
+import MainSecondPage from '../components/MainPage/MainSecondPage';
 import Container from '../components/Container';
+import styled from 'styled-components';
+import MainThirdPage from '../components/MainPage/MainThirdPage';
+import Footer from '../components/Footer';
+
+const SContainer = styled(Container)`
+  display: flex;
+  flex-direction: column;
+`;
 
 const MainPage = () => {
-  return <Container>메인페이지!</Container>;
+  return (
+    <SContainer>
+      <MainFirstPage />
+      <MainSecondPage />
+      <MainThirdPage />
+      <Footer />
+    </SContainer>
+  );
 };
 
 export default MainPage;
