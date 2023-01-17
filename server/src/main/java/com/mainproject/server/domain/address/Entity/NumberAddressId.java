b5a7524eff1ba8ebbd5be1ae3414bd097a812dcb
jjunghakim <111368350+jjunghakim@users.noreply.github.com>
2023-01-17 16:32:16 +0900

    [BE]feat: address 관련 코드 생성 (#105)

---
+++ server/src/main/java/com/mainproject/server/domain/address/Entity/NumberAddressId.java
@@ -0,0 +1,11 @@
+package com.mainproject.server.domain.address.Entity;
+
+import java.io.Serializable;
+
+public class NumberAddressId implements Serializable {
+	private String road_nm_cd;
+	private String ugrd_yn;
+	private String bldg_born;
+	private String bldg_bubn;
+	private String arno_seq;
+}
