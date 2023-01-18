adc6a83dc67c7a59c79d0a7fce780ba63197e42c
alsry922 <alsry922@gmail.com>
2023-01-18 12:08:36 +0900

    [BE] fix: 메일 수정

--- server/src/main/java/com/mainproject/server/helper/email/EmailService.java
+++ server/src/main/java/com/mainproject/server/helper/email/EmailService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.util.Random;
 
@@ -18,27 +19,23 @@ public class EmailService {
     private final JavaMailSender emailSender;
     private final TemplateEngine templateEngine;
 
-    public String sendEmail(String destEmail)  {
+    public String sendEmail(String destEmail) throws MessagingException {
         String verificationCode = createVerificationCode();
-        try {
-            Context context = new Context();
-            context.setVariable("verificationCode", verificationCode);
+        Context context = new Context();
+        context.setVariable("verificationCode", verificationCode);
 
-            MimeMessage mimeMessage = emailSender.createMimeMessage();
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+        MimeMessage mimeMessage = emailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
 
 
-            String mailTemplate = templateEngine.process("mailTemplate", context);
-            mimeMessageHelper.setFrom("seb41mainui@gmail.com");
-            mimeMessageHelper.setTo(destEmail);
-            mimeMessageHelper.setSubject("안녕하세요 킁킁메이트입니다. 회원 가입 이메일 인증 코드를 입력해주세요.");
-            mimeMessageHelper.setText(mailTemplate, true);
+        String mailTemplate = templateEngine.process("mailTemplate", context);
+        mimeMessageHelper.setTo(destEmail);
+        mimeMessageHelper.setSubject("안녕하세요 킁킁메이트입니다. 회원 가입 이메일 인증 코드를 입력해주세요.");
+        mimeMessageHelper.setText(mailTemplate, true);
 
-            emailSender.send(mimeMessage);
-            log.info("Success sending email");
-        } catch (Exception e) {
-            log.error("email send error={}", e.getMessage());
-        }
+        emailSender.send(mimeMessage);
+        log.info("Success sending email");
+
         return verificationCode;
     }
 
@@ -46,15 +43,15 @@ public class EmailService {
         Random random = new Random();
         StringBuffer key = new StringBuffer();
 
-        for(int i=0;i<8;i++) {
+        for (int i = 0; i < 8; i++) {
             int index = random.nextInt(3);
 
             switch (index) {
-                case 0 :
-                    key.append((char) ((int)random.nextInt(26) + 97)); // a~z
+                case 0:
+                    key.append((char) ((int) random.nextInt(26) + 97)); // a~z
                     break;
                 case 1:
-                    key.append((char) ((int)random.nextInt(26) + 65)); // A~Z
+                    key.append((char) ((int) random.nextInt(26) + 65)); // A~Z
                     break;
                 case 2:
                     key.append(random.nextInt(9)); // 0~9
