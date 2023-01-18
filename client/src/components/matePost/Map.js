3dd09f8ca93861cda79162a4fbe4d1f39e9b9e38
syk506 <suuunyy0@naver.com>
2023-01-18 16:07:32 +0900

    [FE] feat: 지도 클릭으로 장소 지정 가능한 기능 구현

--- client/src/components/matePost/Map.js
+++ client/src/components/matePost/Map.js
@@ -1,6 +1,18 @@
+import styled from 'styled-components';
 import { useEffect } from 'react';
-
 const { kakao } = window;
+
+const MapBox = styled.div`
+  width: 100%;
+  display: inline-block;
+  margin-left: 5px;
+  margin-right: 5px;
+`;
+
+const MapContent = styled.div`
+  width: 99%;
+  height: 500px;
+`;
 
 const Map = ({ searchPlace }) => {
   useEffect(() => {
@@ -11,11 +23,24 @@ const Map = ({ searchPlace }) => {
       level: 3,
     };
     const map = new kakao.maps.Map(container, options);
-
     const ps = new kakao.maps.services.Places();
 
+    // 마커 커스텀
+    const imgSrc =
+        'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fb9mhL8%2FbtrWBcjqH22%2FEERoT6xVXkSTCAwvT5X7Z0%2Fimg.png',
+      imgSize = new kakao.maps.Size(45, 50),
+      imgOption = { offset: new kakao.maps.Point(27, 69) };
+
+    const markerImg = new kakao.maps.MarkerImage(imgSrc, imgSize, imgOption),
+      markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+
+    const marker = new kakao.maps.Marker({
+      position: markerPosition,
+      image: markerImg,
+    });
+
+    // 키워드로 장소 검색
     const placesSearchCB = (data, status) => {
-      console.log('data', data);
       if (status === kakao.maps.services.Status.OK) {
         const bounds = new kakao.maps.LatLngBounds();
 
@@ -29,11 +54,12 @@ const Map = ({ searchPlace }) => {
 
     ps.keywordSearch(searchPlace, placesSearchCB);
 
-    // 지도 위에 마커 표시
+    // 지도 위에 마커 표시 & 마커 클릭 시 인포윈도우 표시
     const displayMarker = (place) => {
       const marker = new kakao.maps.Marker({
         map: map,
         position: new kakao.maps.LatLng(place.y, place.x),
+        image: markerImg,
       });
 
       kakao.maps.event.addListener(marker, 'click', function () {
@@ -50,19 +76,36 @@ const Map = ({ searchPlace }) => {
         infowindow.open(map, marker);
       });
     };
+
+    // 지도 클릭으로 장소 지정
+    const geocoder = new kakao.maps.services.Geocoder();
+
+    kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
+      searchDetailAddrFromCoords(mouseEvent.latLng, function (result, status) {
+        if (status === kakao.maps.services.Status.OK) {
+          const content =
+            '<div style="padding: 5px; font-size: 12px;">' +
+            result[0].address.address_name +
+            '</div>';
+
+          marker.setPosition(mouseEvent.latLng);
+          marker.setMap(map);
+
+          infowindow.setContent(content);
+          infowindow.open(map, marker);
+        }
+      });
+    });
+
+    const searchDetailAddrFromCoords = (coords, callback) => {
+      geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+    };
   }, [searchPlace]);
 
   return (
-    <div
-      style={{
-        width: '100%',
-        display: 'inline-block',
-        marginLeft: '5px',
-        marginRight: '5px',
-      }}
-    >
-      <div id="map" style={{ width: '99%', height: '500px' }}></div>
-    </div>
+    <MapBox>
+      <MapContent id="map"></MapContent>
+    </MapBox>
   );
 };
 
