1337239fdff52454a61afa3775b1db51bb8905c2
chyerin802 <chyerin802@gmail.com>
2023-01-20 20:25:41 +0900

    [FE] design: 검색 결과 창 마크업

--- client/src/components/findMate/SearchAddressBox.js
+++ client/src/components/findMate/SearchAddressBox.js
@@ -1,21 +1,40 @@
 import styled from 'styled-components';
 import { IoLocationSharp } from 'react-icons/io5';
 import { BiTargetLock } from 'react-icons/bi';
-// import { findMateGet } from '../../api/findMate';
+import { useState } from 'react';
 
-const SearchAddress = ({ setAddress }) => {
-  const handleKeyUp = (e) => {
-    if (e.key === 'Enter') {
-      if (e.target.value === '') {
-        return;
-      }
-      setAddress(e.target.value);
-    }
-  };
-
-  // 현재 위치로 목록 받아오기
+const SearchAddress = ({ setAddress, setBCode }) => {
+  const [addressList, setAddressList] = useState([
+    '안녕하세요',
+    '서울시 송파구 잠실7동',
+    '서울시 송파구 잠실7동',
+    '서울시 송파구 잠실7동',
+    '서울시 송파구 잠실7동',
+    '서울시 송파구 잠실7동',
+    '서울시 송파구 잠실7동',
+    '서울시 송파구 잠실7동',
+    '서울시 송파구 잠실7동',
+  ]);
   const { kakao } = window;
 
+  // 검색어로 주소 정보 받아오기
+  const getAddressCode = (address) => {
+    // 주소 - 좌표 변환 객체
+    const geocoder = new kakao.maps.services.Geocoder();
+
+    // 주소로 좌표 검색하기 -> 법정 코드 값 가져오기
+    geocoder.addressSearch(address, function (result, status) {
+      if (status === kakao.maps.services.Status.OK) {
+        setAddressList(result);
+        console.log('여기!', result);
+        // const bCode = result[0].address.b_code;
+        // console.log(result[0].address.address_name);
+        // console.log(bCode);
+      }
+    });
+  };
+
+  // 현재 위치로 주소 정보 받아오기
   const geoFind = () => {
     // 현재 위치 파악해서 x, y 좌표 얻기
     if (navigator.geolocation) {
@@ -30,7 +49,7 @@ const SearchAddress = ({ setAddress }) => {
 
           const callback = function (result, status) {
             if (status === kakao.maps.services.Status.OK) {
-              console.log('법정 코드', result[0].code);
+              setBCode(result[0].code);
               setAddress(result[0].address_name);
             }
           };
@@ -44,9 +63,22 @@ const SearchAddress = ({ setAddress }) => {
     }
   };
 
+  // 주소 검색
+  const handleSearchAddressKeyUp = (e) => {
+    if (e.key === 'Enter') {
+      if (e.target.value === '') {
+        return;
+      }
+
+      getAddressCode(e.target.value);
+    }
+  };
+
   const handleLocClick = () => {
     geoFind();
   };
+
+  const handleClick = () => {};
 
   return (
     <SearchAddressBox>
@@ -54,12 +86,26 @@ const SearchAddress = ({ setAddress }) => {
       <AdderssInput
         type="text"
         placeholder="시/도 또는 시/군/구 또는 읍/면/동 이름을 검색하세요"
-        onKeyUp={handleKeyUp}
+        onKeyUp={handleSearchAddressKeyUp}
       ></AdderssInput>
       <LocationButton onClick={handleLocClick}>
         <BiTargetLock />
         현재 위치
       </LocationButton>
+      <SearchResultBox>
+        {addressList.length === 0 ? (
+          <div>검색 결과가 없습니다.</div>
+        ) : (
+          <ul>
+            {addressList.map((el, idx) => (
+              <SearchResultItem key={idx} onClick={handleClick}>
+                <IoLocationSharp className="location-icon" />
+                {el}
+              </SearchResultItem>
+            ))}
+          </ul>
+        )}
+      </SearchResultBox>
     </SearchAddressBox>
   );
 };
@@ -109,6 +155,36 @@ const LocationButton = styled.button`
   font-size: 1rem;
   font-weight: 600;
   gap: 8px;
+`;
+
+const SearchResultBox = styled.div`
+  position: absolute;
+  z-index: 9;
+  background-color: white;
+  width: 100%;
+  height: max-content;
+  top: 3.6rem;
+  box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.1);
+  padding: 1rem 1.25rem;
+  border-radius: 10px;
+
+  > div {
+    width: 100%;
+    color: var(--sec-color);
+  }
+`;
+
+const SearchResultItem = styled.div`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  padding: 0.5rem 0;
+  gap: 0.8rem;
+  color: var(--sec-color);
+
+  &:hover {
+    color: var(--main-font-color);
+  }
 `;
 
 export default SearchAddress;
