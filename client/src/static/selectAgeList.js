299a906026acf1dc36e9413a283a70f513421e93
chyerin802 <chyerin802@gmail.com>
2023-01-24 09:48:01 +0900

    [FE] feat: 나이 선택 옵션 리스트

---
+++ client/src/static/selectAgeList.js
@@ -0,0 +1,8 @@
+const selectAgeList = [
+  { id: 1, name: 'memberAge', value: 'TEENS', title: '10대' },
+  { id: 2, name: 'memberAge', value: 'TWENTIES', title: '20대' },
+  { id: 3, name: 'memberAge', value: 'THIRTIES', title: '30대' },
+  { id: 4, name: 'memberAge', value: 'FORTIES', title: '40대 이상' },
+];
+
+export default selectAgeList;
