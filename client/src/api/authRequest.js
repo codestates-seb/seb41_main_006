a7b42c1abd1ab89ec9f1b36c469a9cb8d406a191
chyerin802 <chyerin802@gmail.com>
2023-01-25 05:54:22 +0900

    [FE] Axios 전역 설정

---
+++ client/src/api/authRequest.js
@@ -0,0 +1,9 @@
+import defalutRequest from './defalutRequest';
+
+const authRequest = defalutRequest.create({
+  // headers: {
+  //   Authorization: `Bearer ${Token}`,
+  // },
+});
+
+export default authRequest;
