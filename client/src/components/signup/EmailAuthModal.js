bb290c85b7256003741b850e2b7adfea87e8d2a5
chyerin802 <chyerin802@gmail.com>
2023-01-24 15:53:25 +0900

    [FE] feat: 인증 메일 재전송 기능

--- client/src/components/signup/EmailAuthModal.js
+++ client/src/components/signup/EmailAuthModal.js
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 import ModalBackDrop from '../ModalBackDrop';
 import Title from '../common/Title';
 import Button from '../common/Button';
+import DogFootLoading from '../DogFootLoading';
 import useInput from '../../hooks/useInput';
-import { authEmailVerification } from '../../api/auth/signup';
+import { authEmailVerification, authEmail } from '../../api/auth/signup';
 
 const EmailAuthModalView = styled.div`
   width: 30rem;
@@ -62,6 +63,8 @@ const EmailAuthModal = ({
   email,
   setIsEmailAuthModalOpen,
   setIsEmailVerified,
+  isEmailLoading,
+  setIsEmailLoading,
 }) => {
   const emailAuthNumber = useInput('');
 
@@ -86,28 +89,50 @@ const EmailAuthModal = ({
     // 인증 번호가 맞지 않을 때의 경우 추가되어야 함!
   };
 
+  const handleReSendEmail = async () => {
+    // 이메일 요청 중이라면..
+    if (isEmailLoading) return;
+
+    setIsEmailLoading(true);
+    try {
+      await authEmail(email);
+      alert('해당 메일로 인증 메일을 전송했습니다.');
+    } catch (error) {
+      console.log(error);
+    }
+    setIsEmailLoading(false);
+  };
+
   return (
     <ModalBackDrop onClick={() => setIsEmailAuthModalOpen(false)}>
       <EmailAuthModalView onClick={(e) => e.stopPropagation()}>
-        <Title as="h2">인증 번호 입력</Title>
-        <div className="auth-guide">
-          <span>{email}</span>로 인증 메일이 전송되었습니다.
-          <div>인증 번호를 입력한 후 완료를 눌러주세요.</div>
-        </div>
-        <EmailAuthInputBox>
-          <input
-            value={emailAuthNumber.value}
-            type="text"
-            onChange={emailAuthNumber.handleChange}
-          ></input>
-          <div>
-            <span className="error">{emailAuthNumber.error}</span>
-            <button type="button">인증번호 재전송</button>
-          </div>
-        </EmailAuthInputBox>
-        <Button onClick={handleEmailAuthSubmit} type="button">
-          완료
-        </Button>
+        {isEmailLoading ? (
+          <DogFootLoading size="2rem" />
+        ) : (
+          <>
+            <Title as="h2">인증 번호 입력</Title>
+            <div className="auth-guide">
+              <span>{email}</span>로 인증 메일이 전송되었습니다.
+              <div>인증 번호를 입력한 후 완료를 눌러주세요.</div>
+            </div>
+            <EmailAuthInputBox>
+              <input
+                value={emailAuthNumber.value}
+                type="text"
+                onChange={emailAuthNumber.handleChange}
+              ></input>
+              <div>
+                <span className="error">{emailAuthNumber.error}</span>
+                <button type="button" onClick={handleReSendEmail}>
+                  인증번호 재전송
+                </button>
+              </div>
+            </EmailAuthInputBox>
+            <Button onClick={handleEmailAuthSubmit} type="button">
+              완료
+            </Button>
+          </>
+        )}
       </EmailAuthModalView>
     </ModalBackDrop>
   );
