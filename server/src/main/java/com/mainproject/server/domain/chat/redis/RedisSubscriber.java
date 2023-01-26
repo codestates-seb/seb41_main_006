d589745d804b750efc59c2ba82e79710bc3e755f
hiy7030 <hiy7030@gmail.com>
2023-01-26 15:35:44 +0900

    #164 [BE]fix: publish message 객체화 클래스 생성

--- server/src/main/java/com/mainproject/server/domain/chat/redis/RedisSubscriber.java
+++ server/src/main/java/com/mainproject/server/domain/chat/redis/RedisSubscriber.java
@@ -1,12 +1,9 @@
 package com.mainproject.server.domain.chat.redis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mainproject.server.domain.chat.entity.ChatMessage;
+import com.mainproject.server.domain.chat.entity.RedisChat;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.connection.MessageListener;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
@@ -21,9 +18,10 @@ public class RedisSubscriber{
     public void sendMessage(String publishMessage) {
         try{
             // 레디스를 통해 들어온 메세지를 chatMessage로 변환
-            ChatMessage chatMessage = objectMapper.readValue(publishMessage, ChatMessage.class);
+            RedisChat chatMessage = objectMapper.readValue(publishMessage, RedisChat.class);
             // 채팅방을 구독하고 있는 회원에게 해당 메세지를 뿌림
-            messagingTemplate.convertAndSend("/sub/chats/" + chatMessage.getChatRoom().getRoomId(), chatMessage);
+            messagingTemplate.convertAndSend("/sub/chats/" + chatMessage.getRoomId(), chatMessage);
+            log.info("redis publish messages");
         } catch (Exception e) {
             log.error("Exception in redis Subscriber:{}", e);
         }
