6a28341e7d0272ac475c2a4b898fa2b98794765f
chyerin802 <chyerin802@gmail.com>
2023-01-27 02:21:40 +0900

    [FE] fix: access Token 오류 해결

--- client/src/api/authRequest.js
+++ client/src/api/authRequest.js
@@ -1,10 +1,22 @@
 import defaultRequest from './defaultRequest';
 
-const authRequest = defaultRequest.create({
-  headers: {
-    Authorization: `${localStorage.getItem('AccessToken')}`,
+const authRequest = defaultRequest.create();
+
+authRequest.interceptors.request.use(
+  function (config) {
+    // 요청을 보내기 전에 헤더에 토큰값 설정
+    const AUTH_TOKEN = localStorage.getItem('AccessToken');
+    config.headers['Authorization'] = AUTH_TOKEN;
+
+    return config;
   },
-});
+  function (error) {
+    // 오류 요청을 보내기전 수행할 일
+    // ...
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
 
 authRequest.interceptors.response.use(
   function (response) {
@@ -24,9 +36,9 @@ authRequest.interceptors.response.use(
     if (error.response.status === 401) {
       const refreshToken = localStorage.getItem('refreshToken');
       if (refreshToken) {
-        authRequest
+        defaultRequest
           .post(
-            '/reissue',
+            '/auth/reissue',
             {},
             {
               headers: { Refresh: refreshToken },
@@ -35,6 +47,14 @@ authRequest.interceptors.response.use(
           .then((res) => {
             localStorage.setItem('AccessToken', res.headers.authorization);
             window.location.reload();
+          })
+          .catch((error) => {
+            if (error.response.status === 401) {
+              localStorage.removeItem('AccessToken');
+              localStorage.removeItem('refreshToken');
+              alert('다시 로그인 해 주세요');
+              window.location.reload();
+            }
           });
       } else {
         return alert('로그인 후 이용해 주세요');
