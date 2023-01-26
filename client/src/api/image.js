e148bed89f62d9b56e7ff532a5fccde8e4e566f5
chyerin802 <chyerin802@gmail.com>
2023-01-26 13:11:42 +0900

    [FE] 이미지 업로드 테스트 완료

--- client/src/api/image.js
+++ client/src/api/image.js
@@ -1,11 +1,22 @@
 import defalutRequest from './defaultRequest';
+
 // 멤버 이미지 업로드 /s3/member
-export const memberImageUpload = (file) => {
-  console.log(file);
-  // return instance.post(...)
-  return defalutRequest.post('/s3/member', file, {
+export const memberImageUpload = async (file) => {
+  const formData = new FormData();
+  formData.append('images', file);
+  let entries = formData.entries();
+  for (const pair of entries) {
+    console.log(pair[0] + ', ' + pair[1]);
+  }
+  return defalutRequest.post('/s3/member', formData, {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
+  });
+};
+
+export const memberImageDelete = (fileUrl) => {
+  return defalutRequest.delete('/s3/member', {
+    params: { upFileUrl: fileUrl },
   });
 };
