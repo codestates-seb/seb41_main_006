2183d3b3468dadcac123b65e493e6c31545568c2
chyerin802 <chyerin802@gmail.com>
2023-01-28 21:17:08 +0900

    [FE] fix: date conver 수정

--- client/src/utils/dateConvert.js
+++ client/src/utils/dateConvert.js
@@ -21,13 +21,8 @@ export const convertAppointDate = (appointTime) => {
   const month = String(utcDate.getMonth() + 1).padStart(2, '0');
   let date = String(utcDate.getDate()).padStart(2, '0');
   const day = weekday[utcDate.getDay()];
-  let hours = utcDate.getHours() + 9;
+  let hours = utcDate.getHours();
   const minutes = utcDate.getMinutes();
-
-  if (hours >= 24) {
-    date = Number(date) + 1;
-    hours = Number(hours) - 24;
-  }
 
   const time = `${hours <= 12 ? `오전 ${hours}시` : `오후 ${hours - 12}시`} ${
     minutes === 0 ? '' : `${minutes}분`
