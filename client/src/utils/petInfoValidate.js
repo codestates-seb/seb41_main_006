7b5ad240546c32433a6efcf0e5ec4d770c689f0f
chyerin802 <chyerin802@gmail.com>
2023-01-29 23:26:09 +0900

    [FE] 멤버, 펫 이름  글자 수 제한

--- client/src/utils/petInfoValidate.js
+++ client/src/utils/petInfoValidate.js
@@ -1,9 +1,19 @@
-export default function petInfoValidate({ name, age, gender, breed, petSize }) {
+export default function petInfoValidate({
+  name,
+  age,
+  gender,
+  breed,
+  petSize,
+  aboutDog,
+}) {
   const errors = {};
 
   if (!name) {
     errors.name = '필수 항목입니다.';
+  } else if (name.length > 10) {
+    errors.name = '이름은 10자 이내로 작성해주세요';
   }
+
   if (!age) {
     errors.age = '필수 항목입니다.';
   }
@@ -16,6 +26,8 @@ export default function petInfoValidate({ name, age, gender, breed, petSize }) {
   if (!petSize) {
     errors.petSize = '필수 항목입니다.';
   }
-
+  if (aboutDog.length > 40) {
+    errors.aboutDog = '40자를 초과하였습니다.';
+  }
   return errors;
 }
