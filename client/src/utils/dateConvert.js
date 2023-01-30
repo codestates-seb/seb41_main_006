2f97c86e566aa9a5ee2d7adbd0ccdd5180a94907
syk506 <suuunyy0@naver.com>
2023-01-31 00:28:25 +0900

    [FE] fix: 만나는 시간 한국 시간 기준으로 수정

--- client/src/utils/dateConvert.js
+++ client/src/utils/dateConvert.js
@@ -29,3 +29,25 @@ export const convertAppointDate = (appointTime) => {
 
   return `${year}.${month}.${date} (${day}) ${time}`;
 };
+
+export const uctTokst = (appointTime) => {
+  const utcDate = new Date(appointTime);
+
+  const weekday = ['일', '월', '화', '수', '목', '금', '토'];
+
+  const plusNineHours = 1 * 60 * 60 * 1000 * 9;
+  utcDate.setTime(utcDate.getTime() + plusNineHours);
+
+  const year = utcDate.getFullYear();
+  const month = String(utcDate.getMonth() + 1).padStart(2, '0');
+  let date = String(utcDate.getDate()).padStart(2, '0');
+  const day = weekday[utcDate.getDay()];
+  let hours = utcDate.getHours();
+  const minutes = utcDate.getMinutes();
+
+  const time = `${hours <= 12 ? `오전 ${hours}시` : `오후 ${hours - 12}시`} ${
+    minutes === 0 ? '' : `${minutes}분`
+  }`;
+
+  return `${year}.${month}.${date} (${day}) ${time}`;
+};
