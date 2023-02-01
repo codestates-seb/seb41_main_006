fc8b5f71c2133f36ffcbaef7eea5d15b1c71b43b
operat04 <operat04@naver.com>
2023-02-01 18:11:06 +0900

    [FE]fix : favicon 추가

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -32,6 +32,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      favicon: './src/logo.png',
     }),
     new Dotenv({
       path: './.env',
